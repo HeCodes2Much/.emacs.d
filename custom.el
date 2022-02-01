1e5070a84b06c77810c2b7f487582801044c6cbe
The-Repo-Club <wayne6324@gmail.com>
2022-02-01 22:43:52 +0000

    INIT

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-battery-mode t)
+ '(display-line-numbers-type 'visual)
+ '(display-time-mode t)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages
+   '(tree-sitter-langs tree-sitter undo-tree dracula-theme evil bazel company helm-company spaceline diminish perspective terraform auto-package-update use-package blacken all-the-icons neotree yasnippet git-commit hcl-mode helm helm-core markdown-mode projectile scala-mode yaml-mode thrift terraform-mode rg rainbow-delimiters python-mode puppet-mode protobuf-mode powerline magit json-mode helm-projectile helm-descbinds helm-xref dumb-jump bazel-mode bar-cursor autopair auto-virtualenvwrapper auctex))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(transient-mark-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
