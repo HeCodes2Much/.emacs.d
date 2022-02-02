11767c43fa7e6e2ce7395ee2fe118a5b963b0d93
The-Repo-Club <wayne6324@gmail.com>
2022-02-02 00:27:37 +0000

    Added a dashboard
    Signed-off-by: The-Repo-Club <wayne6324@gmail.com>

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter undo-tree dracula-theme evil bazel company helm-company spaceline diminish perspective terraform auto-package-update use-package blacken all-the-icons neotree yasnippet git-commit hcl-mode helm helm-core markdown-mode projectile scala-mode yaml-mode thrift terraform-mode rg rainbow-delimiters python-mode puppet-mode protobuf-mode powerline magit json-mode helm-projectile helm-descbinds helm-xref dumb-jump bazel-mode bar-cursor autopair auto-virtualenvwrapper auctex)))
+   '(dashboard-ls tree-sitter-langs tree-sitter undo-tree dracula-theme evil bazel company helm-company spaceline diminish perspective terraform auto-package-update use-package blacken all-the-icons neotree yasnippet git-commit hcl-mode helm helm-core markdown-mode projectile scala-mode yaml-mode thrift terraform-mode rg rainbow-delimiters python-mode puppet-mode protobuf-mode powerline magit json-mode helm-projectile helm-descbinds helm-xref dumb-jump bazel-mode bar-cursor autopair auto-virtualenvwrapper auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
